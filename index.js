15ed8031a65076c9562a129856e0c6128ede55ce
adastrix <adastrix@gmail.com>
2018-05-30 20:39:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hola Getafe!');
+  res.send('Hola Mundo!');
 });
 
 var server = app.listen(3000, function () {
